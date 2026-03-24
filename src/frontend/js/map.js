dc60f767720bbc58331d48ecc83e50687e7a2d1c
ToombaYoomba <z.morkhov@gmail.com>
2026-03-24 15:20:02 +0300

    Add map to the project

--- src/frontend/js/map.js
+++ src/frontend/js/map.js
@@ -1,1 +1,10 @@
 // Управление картой с помощью Leaflet
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+marker.bindPopup("<b>Hello World!</b><br>I am a popup.").openPopup();

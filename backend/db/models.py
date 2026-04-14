9d3716a506adcd5823c1775106d8a2d86d0fedff
miniMlena <milena.muhamor@gmail.com>
2026-04-14 23:30:56 +0300

    MM-BE-01 (#29)
    * Delete route_points and distances tables
    
    * Delete dependencies from old tables
    
    * Remove list convertations

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Float, ForeignKey, Integer
+from sqlalchemy import Float, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import JSON
 
@@ -19,24 +19,5 @@ class RouteModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     points: Mapped[list[int]] = mapped_column(JSON, nullable=False)
     coordinates: Mapped[list[list[float]]] = mapped_column(JSON, nullable=False)
-    distance_km: Mapped[float] = mapped_column(Float, nullable=False)
-    duration_minutes: Mapped[float] = mapped_column(Float, nullable=False)
-
-
-class RoutePointModel(Base):
-    __tablename__ = "route_points"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    route_id: Mapped[int] = mapped_column(ForeignKey("routes.id"), nullable=False)
-    point_id: Mapped[int] = mapped_column(ForeignKey("points.id"), nullable=False)
-    sequence: Mapped[int] = mapped_column(Integer, nullable=False)
-
-
-class DistanceModel(Base):
-    __tablename__ = "distances"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    from_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"), nullable=False)
-    to_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"), nullable=False)
     distance_km: Mapped[float] = mapped_column(Float, nullable=False)
     duration_minutes: Mapped[float] = mapped_column(Float, nullable=False)
--- backend/domain/route_point.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class RoutePoint:
-    route_id: int
-    point_id: int
-    sequence: int
